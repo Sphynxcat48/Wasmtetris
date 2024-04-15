8f90f4bfa095aaef013f40fa952e150ed188506b
Sphynxcat48 <123120505+Sphynxcat48@users.noreply.github.com>
2024-04-15 09:39:58 -0600

    Update render.js

--- render.js
+++ render.js
@@ -1,1 +1,16 @@
+const canvas = document.getElementById("webglCanvas");
 
+
+// Get the WebGL rendering context
+const gl = canvas.getContext("webgl");
+
+
+// Check if WebGL is available on the current browser
+if (!gl) {
+  alert("WebGL is not supported on this browser.");
+}
+
+
+// Set clear color to black, and clear the canvas
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
