81398f4c0e12e492df17adfb6f92910040156bd7
Sphynxcat48 <123120505+Sphynxcat48@users.noreply.github.com>
2024-04-16 08:21:37 -0600

    Update render.js

--- render.js
+++ render.js
@@ -195,6 +195,77 @@ class LPiece extends TetrisPiece {
         super(shape, color);
     }
 }
+class SPiece extends TetrisPiece {
+    constructor() {
+        const shape = [
+            [[0, 1, 1],
+             [1, 1, 0]],
+            [[1, 0],
+             [1, 1],
+             [0, 1]]
+        ];
+        const color = [0, 1, 0, 1]; // green
+        super(shape, color);
+    }
+}
+
+class ZPiece extends TetrisPiece {
+    constructor() {
+        const shape = [
+            [[1, 1, 0],
+             [0, 1, 1]],
+            [[0, 1],
+             [1, 1],
+             [1, 0]]
+        ];
+        const color = [1, 0, 0, 1]; // red
+        super(shape, color);
+    }
+}
+
+class TPiece extends TetrisPiece {
+    constructor() {
+        const shape = [
+            [[0, 1, 0],
+             [1, 1, 1]],
+            [[1, 0],
+             [1, 1],
+             [1, 0]],
+            [[1, 1, 1],
+             [0, 1, 0]],
+            [[0, 1],
+             [1, 1],
+             [0, 1]]
+        ];
+        const color = [0.541, 0.169, 0.886, 1]; // purple
+        super(shape, color);
+    }
+}
+
+class IPiece extends TetrisPiece {
+    constructor() {
+        const shape = [
+            [[1],
+             [1],
+             [1],
+             [1]],
+            [[1, 1, 1, 1]]
+        ];
+        const color = [0, 1, 1, 1]; // cyan
+        super(shape, color);
+    }
+}
+
+class OPiece extends TetrisPiece {
+    constructor() {
+        const shape = [
+            [[1, 1],
+             [1, 1]]
+        ];
+        const color = [1, 1, 0, 1]; // yellow
+        super(shape, color);
+    }
+}
 
 class JPiece extends TetrisPiece {
     constructor() {
